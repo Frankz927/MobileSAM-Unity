8f3168fa9cc7b85222bd95da374c6c892698ce8a
Frank <39018610+Frankz927@users.noreply.github.com>
2024-10-19 16:25:54 +0900

    たのむ

--- MobileSAM Project/Assets/Samples/SampleApp/Scripts/Segmentation/DeviceManagerEditor.cs
+++ MobileSAM Project/Assets/Samples/SampleApp/Scripts/Segmentation/DeviceManagerEditor.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
 [CustomEditor(typeof(DeviceManager))]
 public class DeviceManagerEditor : Editor
 {
@@ -40,3 +41,4 @@ public class DeviceManagerEditor : Editor
         DrawDefaultInspector();
     }
 }
+#endif
